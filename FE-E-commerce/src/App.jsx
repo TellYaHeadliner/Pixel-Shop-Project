3c26ae422074bed529be62e1a0079e7a0c7786f5
TellYaHeadliner <buibaolam0302@gmail.com>
2024-12-25 17:20:21 +0700

    Add: Component Header, ButtonNav, Search (25%)

--- FE-E-commerce/src/App.jsx
+++ FE-E-commerce/src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter } from 'react-router';
 import AppRoutes from './routes/AppRoute';
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
 
 function App() {
   return (
--- FE-E-commerce/src/components/Button/ButtonNav.jsx
+++ FE-E-commerce/src/components/Button/ButtonNav.jsx
@@ -1,22 +1,42 @@
 import { BsFillTelephoneFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
+import PropTypes from "prop-types"; 
 
-export default function Button(){
-    return (
-        <div className="button">
-            <div style={{
-                'width': '50px',
-                'height': '50px',
-                'borderRadius': '8px',
-                'backgroundColor': '#f8f9fa',
-                'display': 'flex',
-                'justifyContent': 'center',
-                'alignItems': 'center',
-                'border': '1px solid #ddd'
-            }}>
-                <BsFillTelephoneFill size="24px" color="#333"/>
-                <span style={{ fontSize: '14px', marginTop: '5px'}}>Liên hệ</span>
-            </div>
-        </div>            
-    )
- 
-}
+import styles from "./ButtonNav.module.css";
+
+const mapIcon = {
+  "Liên hệ": BsFillTelephoneFill,
+  "Giỏ hàng": FiShoppingCart,
+  "Đăng nhập/Đăng ký": FaUser,
+};
+
+
+
+const ButtonNav = ({ name }) => {
+  const IconComponent = mapIcon[name];
+  
+  const iconClassName =
+    name === "Giỏ hàng" ? styles.cart : 
+    name === "Liên hệ" ? styles.contact : 
+    name === "Đăng nhập/Đăng ký" ? styles.user : "";
+
+  return (
+    <div className="button d-flex flex-column align-items-center">
+      {IconComponent && (
+        <span className={`${styles.icon} ${iconClassName}`}>
+          <IconComponent size="24px" color="#00" />
+        </span>
+      )}
+
+      <span>{name}</span>
+    </div>
+  );
+};
+
+ButtonNav.propTypes = {
+  name: PropTypes.string.isRequired, // Ensure 'name' is a string and required
+};
+
+export default ButtonNav;
+
