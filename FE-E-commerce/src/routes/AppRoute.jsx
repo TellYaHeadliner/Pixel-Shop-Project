ea89c0f09bce8088a23354bc5e85724d22e567df
NhieuThienAn <hhyu03096@gmail.com>
2025-01-06 13:40:24 +0700

    nâng hiệu suất bằng lazy trong approute

--- FE-E-commerce/src/routes/AppRoute.jsx
+++ FE-E-commerce/src/routes/AppRoute.jsx
@@ -1,46 +1,50 @@
+import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import ClientLayout from "../components/Client/Layouts/ClientLayout";
-import Home from "../pages/Client/Home/Home";
-import DetailProduct from "../pages/Client/DetailProduct";
+const ClientLayout = lazy(() => import("../components/Client/Layouts/ClientLayout"));
+const Home = lazy(() => import("../pages/Client/Home/Home"));
+const DetailProduct = lazy(() => import("../pages/Client/DetailProduct"));
 
-import AdminLayout from "../components/Admin/Layouts/AdminLayout";
-import SupplierManagement from "../pages/Admin/SupplierManagement/SupplierManagement";
-import CategoryManagement from "../pages/Admin/CategoryManagement/CategoryManagement";
-import ProductManagement from "../pages/Admin/ProductManagement/ProductManagement";
-import BatchManagement from "../pages/Admin/BatchManagement/BatchManagement";
-import CommentManagement from "../pages/Admin/CommentManagement/CommentManagement";
-import AudienceStatistics from "../pages/Admin/AudienceStatistics/AudienceStatistics";
-import OrderStatistics from "../pages/Admin/OrderStatistics/OrderStatistics";
-import Contact from "../pages/Admin/Contact/Contact";
-import WebsiteInfo from "../pages/Admin/WebsiteInfo/WebsiteInfo";
+const AdminLayout = lazy(() => import("../components/Admin/Layouts/AdminLayout"));
+const SupplierManagement = lazy(() => import("../pages/Admin/SupplierManagement/SupplierManagement"));
+const CategoryManagement = lazy(() => import("../pages/Admin/CategoryManagement/CategoryManagement"));
+const ProductManagement = lazy(() => import("../pages/Admin/ProductManagement/ProductManagement"));
+const BatchManagement = lazy(() => import("../pages/Admin/BatchManagement/BatchManagement"));
+const CommentManagement = lazy(() => import("../pages/Admin/CommentManagement/CommentManagement"));
+const AudienceStatistics = lazy(() => import("../pages/Admin/AudienceStatistics/AudienceStatistics"));
+const OrderStatistics = lazy(() => import("../pages/Admin/OrderStatistics/OrderStatistics"));
+const Contact = lazy(() => import("../pages/Admin/Contact/Contact"));
+const WebsiteInfo = lazy(() => import("../pages/Admin/WebsiteInfo/WebsiteInfo"));
 
-import StaffLayout from "../components/Staff/Layouts/StafffLayout";
-import StaffContact from "../pages/Staff/Contact/Contact";
+const StaffLayout = lazy(() => import("../components/Staff/Layouts/StafffLayout"));
+const StaffContact = lazy(() => import("../pages/Staff/Contact/Contact"));
+
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<ClientLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/product" element={<DetailProduct />} />
-      </Route>
-      <Route path="/admin" element={<AdminLayout />}>
-        <Route index element={<WebsiteInfo />} />
-        <Route path="suppliers" element={<SupplierManagement />} />
-        <Route path="categories" element={<CategoryManagement />} />
-        <Route path="products" element={<ProductManagement />} />
-        <Route path="batches" element={<BatchManagement />} />
-        <Route path="comments" element={<CommentManagement />} />
-        <Route path="audience-statistics" element={<AudienceStatistics />} />
-        <Route path="order-statistics" element={<OrderStatistics />} />
-        <Route path="contact" element={<Contact />} />
-      </Route>
-      <Route path="/staff" element={<StaffLayout />}>
-        <Route index element={<StaffContact />} />
-        {/* <Route path="orders" element={<StaffContact />} />
-        <Route path="customer_consulting" element={<StaffContact />} /> */}
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<ClientLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/product" element={<DetailProduct />} />
+        </Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<WebsiteInfo />} />
+          <Route path="suppliers" element={<SupplierManagement />} />
+          <Route path="categories" element={<CategoryManagement />} />
+          <Route path="products" element={<ProductManagement />} />
+          <Route path="batches" element={<BatchManagement />} />
+          <Route path="comments" element={<CommentManagement />} />
+          <Route path="audience-statistics" element={<AudienceStatistics />} />
+          <Route path="order-statistics" element={<OrderStatistics />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+        <Route path="/staff" element={<StaffLayout />}>
+          <Route index element={<StaffContact />} />
+          {/* <Route path="orders" element={<StaffContact />} />
+          <Route path="customer_consulting" element={<StaffContact />} /> */}
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
