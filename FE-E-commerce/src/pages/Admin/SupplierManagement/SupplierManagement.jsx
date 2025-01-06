4a9b270429d5ed28805aea79d3d19e6c94ee604e
Nguyen-Tuan-Kiet27 <0306221133@caothang.edu.vn>
2025-01-07 00:26:19 +0700

    Get ThongTin

--- FE-E-commerce/src/pages/Admin/SupplierManagement/SupplierManagement.jsx
+++ FE-E-commerce/src/pages/Admin/SupplierManagement/SupplierManagement.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './SupplierManagement.scss';
 import { Input, Table, Typography, Row, Col, Button,Form, message } from 'antd';
+import { IndentUI } from 'ckeditor5';
 
 
 const { Title } = Typography;
@@ -33,54 +34,68 @@ const SupplierManagement = () => {
     };
 
     return (
-        <div className='supplier-management'>
+        <div className='supplier-management w-100'>
             <h2 className='heading'>Nhà cung cấp</h2>
             <hr />
-
-            <div className='input-container'>
-                <input
-                    type='text'
-                    name='name'
-                    placeholder='Tên nhà cung cấp'
-                    value={inputs.name}
-                    onChange={handleChange}
-                    className='input-field'
-                />
-                <input
-                    type='text'
-                    name='address'
-                    placeholder='Địa chỉ'
-                    value={inputs.address}
-                    onChange={handleChange}
-                    className='input-field'
-                />
-                <input
-                    type='tel'
-                    name='phone'
-                    placeholder='Số điện thoại'
-                    value={inputs.phone}
-                    onChange={handleChange}
-                    className='input-field'
-                />
-                <input
-                    type='email'
-                    name='email'
-                    placeholder='Email'
-                    value={inputs.email}
-                    onChange={handleChange}
-                    className='input-field'
-                />
-                <input
-                    type='text'
-                    name='product'
-                    placeholder='Sản phẩm cung cấp'
-                    value={inputs.product}
-                    onChange={handleChange}
-                    className='input-field'
-                />
-                <button onClick={handleSubmit} className='add-button'>Thêm</button>
-            </div>
+						<div className='default-bnbg pt-4'>
+								<Form
+									layout="Horizontal"
+								>
+									<div className='d-flex'>
+											<div className='col-6'>
+												<Form.Item
+													name="tenNhaCungCap"
+													label="Tên nhà cung cấp:"
+													labelCol={{span: 7}}
+													wrapperCol={{span: 15}}
+												>
+													<Input/>
+												</Form.Item>
+												<Form.Item
+													name="tenLienHe"
+													label="Tên người liên hệ:"
+													labelCol={{span: 7}}
+													wrapperCol={{span: 15}}
+												>
+													<Input/>
+												</Form.Item>
+												<Form.Item
+													name="diaChi"
+													label="Địa chỉ:"
+													labelCol={{span: 7}}
+													wrapperCol={{span: 15}}
+												>
+													<Input/>
+												</Form.Item>
+											</div>
+											<div className='col-6'>
+												<Form.Item
+													name="soDienThoai"
+													label="Số điện thoại:"
+													labelCol={{span: 7}}
+													wrapperCol={{span: 15}}
+												>
+													<Input/>
+												</Form.Item>
+												<Form.Item
+													name="email"
+													label="Email:"
+													labelCol={{span: 7}}
+													wrapperCol={{span: 15}}
+												>
+													<Input/>
+												</Form.Item>
+												<div className='d-flex justify-content-end buttonAdd'>
+													<Button
+														htmlType='submit'
+													>Thêm</Button>
+												</div>
+											</div>
+									</div>
+								</Form>
+						</div>
             <hr />
+						
             <div className='suppliers-list'>
                 <h3 className='list-heading'>Danh sách nhà cung cấp</h3>
                 <div className='columns'>
